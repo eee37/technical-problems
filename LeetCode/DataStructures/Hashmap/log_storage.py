4fef9f269ac82c24b6ea3d468d767839d8852551
Eric Enriquez <ericenriquez@Erics-MBP.lan>
2022-02-18 18:00:36 -0500

    new problem

---
+++ LeetCode/DataStructures/Hashmap/log_storage.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+
+class LogSystem:
+
+    def __init__(self):
+        self.hist = []
+        self.granularity_index_map = dict({ # NOTE: Py Syntax String keys need to be wrapped in quotes
+            "Year": 3,
+            "Month": 6,
+            "Day": 9,
+            "Hour": 12,
+            "Minute": 15,
+            "Second": 18
+        })
+        
+        self.regex_map = dict({
+            "Year": '%Y',
+            "Month": '%Y:%m',
+            "Day": '%Y:%m:%d',
+            "Hour": '%Y:%m:%d:%H',
+            "Minute": '%Y:%m:%d:%H:%M',
+            "Second": '%Y:%m:%d:%H:%M:%S'
+        })
+        
+
+    def put(self, id: int, timestamp: str) -> None:
+        # https://stackoverflow.com/questions/5292303/how-does-tuple-comparison-work-in-python
+        # heapq.heappush(self.heap, (timestamp, id) )  # NOTE: Look into how tuples are compared
+        # NOTE: Can consider removing sorting here. Can improve to O(1) at no cost to retrieve
+        self.hist.append((timestamp, id))
+        
+    def convert_string_date(self, string_date: str, regex_date: str):
+        return datetime.strptime(string_date, regex_date)
+    
+    def retrieve(self, start: str, end: str, granularity: str) -> List[int]:
+        last_index_match = self.granularity_index_map[granularity]
+        
+        regex_date = self.regex_map[granularity]
+        start = self.convert_string_date(start[:last_index_match + 1], regex_date) # NOTE: Py Syntax You can slice string just not reassing value at index cause strings are immutable. slicing just creates a new string
+        end = self.convert_string_date(end[:last_index_match + 1], regex_date)
+        ids_response = []
+        for index, log in enumerate(self.hist):
+            datetime, id  = log
+            
+            datetime = self.convert_string_date(datetime[: last_index_match + 1], regex_date)
+            if start <= datetime and end >= datetime:
+                ids_response.append(id)
+        return ids_response
+                
+                
+        
+        
+
+
+# Your LogSystem object will be instantiated and called as such:
+# obj = LogSystem()
+# obj.put(id,timestamp)
+# param_2 = obj.retrieve(start,end,granularity)
